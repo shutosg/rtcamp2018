4c18fc05159610c9ab07e7c0388f2825a977d48a
Shuto <shutoreds@gamil.com>
2018-08-11 19:16:11 +0900

    add: Camera追加

---
+++ RaytracingCamp2018/Camera.cpp
@@ -0,0 +1,33 @@
+﻿#include "Camera.hpp"
+
+
+
+Camera::Camera()
+{
+}
+
+Camera::Camera(
+    const Vec & pos,
+    const Vec & dir,
+    const Vec & up,
+    double fov,
+    double focalDist,
+    double aspect)
+{
+    eye = pos;
+    xAxis = dir.cross(up).normalize();
+    yAxis = up.normalize();
+    double halfWidth = focalDist * std::tan(fov * 0.5);
+    centerDir = dir.normalize().scale(focalDist);
+    this->aspect = aspect;
+}
+
+Ray Camera::getPrimaryRay(double x, double y)
+{
+    auto dir = centerDir + xAxis * x * aspect + yAxis * y;
+    return Ray(eye, dir);
+}
+
+Camera::~Camera()
+{
+}
---
+++ RaytracingCamp2018/Camera.hpp
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "Vec.hpp"
+#include "Ray.hpp"
+
+class Camera
+{
+public:
+    Camera();
+    Camera
+    (
+        const Vec &pos,
+        const Vec &dir,
+        const Vec &up,
+        double fov,
+        double focalDist,
+        double aspect
+    );
+    Ray getPrimaryRay(double x, double y);
+    ~Camera();
+private:
+    Vec eye;
+    Vec xAxis;
+    Vec yAxis;
+    Vec centerDir;
+    double aspect;
+};
+
