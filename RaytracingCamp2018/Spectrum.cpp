2b28414f0dd843406f7e82e130835d07f8058f3a
Shuto <shutoreds@gamil.com>
2018-08-11 19:11:25 +0900

    add: Spectrum追加

---
+++ RaytracingCamp2018/Spectrum.cpp
@@ -0,0 +1,66 @@
+﻿#include "Spectrum.hpp"
+
+Spectrum::Spectrum()
+{
+    r = g = b = 0;
+}
+
+Spectrum::Spectrum(double r, double g, double b)
+{
+    this->r = r;
+    this->g = g;
+    this->b = b;
+}
+
+Spectrum::Spectrum(double v)
+{
+    this->r = v;
+    this->g = v;
+    this->b = v;
+}
+
+Spectrum Spectrum::scale(const double s)
+{
+    return Spectrum(this->r * s, this->g * s, this->b * s);
+}
+
+
+Spectrum Spectrum::operator+(const Spectrum &s)
+{
+    return Spectrum(this->r + s.r, this->g + s.g, this->b + s.b);
+}
+
+Spectrum Spectrum::operator-(const Spectrum &s)
+{
+    return Spectrum(this->r - s.r, this->g - s.g, this->b - s.b);
+}
+
+Spectrum Spectrum::operator/(const Spectrum &s)
+{
+    return Spectrum(this->r / s.r, this->g / s.g, this->b / s.b);
+}
+
+Spectrum Spectrum::operator*(const Spectrum &s)
+{
+    return Spectrum(this->r * s.r, this->g * s.g, this->b * s.b);
+}
+
+void Spectrum::operator=(const Spectrum &s)
+{
+    this->r = s.r;
+    this->g = s.g;
+    this->b * s.b;
+}
+
+Vec Spectrum::toColor()
+{
+    int ir = (int)std::min(std::pow(this->r, 1.0 / DISPLAY_GAMMA) * 255, 255.0);
+    int ig = (int)std::min(std::pow(this->g, 1.0 / DISPLAY_GAMMA) * 255, 255.0);
+    int ib = (int)std::min(std::pow(this->b, 1.0 / DISPLAY_GAMMA) * 255, 255.0);
+    return Vec(ir, ig, ib);
+}
+
+std::string Spectrum::toStr()
+{
+    return "Spectrum(" + std::to_string(r) + ", " + std::to_string(g) + ", " + std::to_string(b) + ")";
+}
---
+++ RaytracingCamp2018/Spectrum.hpp
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <cmath>
+#include <algorithm>
+#include "Vec.hpp"
+#include "constant.h"
+
+class Spectrum
+{
+public:
+    Spectrum();
+    Spectrum(double r, double g, double b);
+    Spectrum(double v);
+    double r, g, b;
+
+    Spectrum scale(const double s);
+    Spectrum operator + (const Spectrum &s);
+    Spectrum operator - (const Spectrum &s);
+    Spectrum operator * (const Spectrum &s);
+    Spectrum operator / (const Spectrum &s);
+    void operator = (const Spectrum &s);
+    Vec toColor();
+    std::string toStr();
+private:
+    const double DISPLAY_GAMMA = 2.2;
+};
