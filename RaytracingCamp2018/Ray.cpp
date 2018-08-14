25c118db694fd376f13d726ea4600a63a202b3d4
Shuto <shutoreds@gamil.com>
2018-08-15 03:44:07 +0900

    add: Ray追加

---
+++ RaytracingCamp2018/Ray.cpp
@@ -0,0 +1,27 @@
+﻿#include "Ray.hpp"
+
+Ray::Ray()
+    : origin(Vec(0))
+    , dir(Vec(0, 0, 1))
+{
+}
+
+Ray::Ray(const Vec &origin, const Vec &dir)
+    : origin(origin)
+    , dir(dir)
+{
+}
+
+Ray::Ray(const Vec &origin, const Vec &dir, bool isAdvanceRay)
+    : origin(origin)
+    , dir(dir)
+{
+    if(isAdvanceRay) {
+        this->origin = origin + dir.scale(constants::kEPS);
+    }
+}
+
+
+Ray::~Ray()
+{
+}
---
+++ RaytracingCamp2018/Ray.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "Vec.hpp"
+#include "constant.h"
+
+class Ray
+{
+public:
+    Ray();
+    Ray(const Vec &origin, const Vec &dir);
+    Ray(const Vec &origin, const Vec &dir, bool isAdvanceRay);
+    ~Ray();
+    Vec origin;
+    Vec dir;
+};
