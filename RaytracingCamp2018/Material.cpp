bb7fbdcf72506198188305636673009012748584
Shuto <shutoreds@gamil.com>
2018-08-15 03:45:24 +0900

    add: Material追加

---
+++ RaytracingCamp2018/Material.cpp
@@ -0,0 +1,32 @@
+﻿#include "Material.hpp"
+
+
+
+Material::Material()
+    : diffuse(new Spectrum(0.8))
+{
+}
+
+Material::Material(const Material &material)
+{
+    diffuse = new Spectrum(
+        material.diffuse->r,
+        material.diffuse->g,
+        material.diffuse->b
+    );
+}
+
+Material::Material(Spectrum *diffuse)
+    : diffuse(diffuse)
+{
+}
+
+Material::~Material()
+{
+    delete diffuse;
+}
+
+void Material::operator=(const Material &m)
+{
+    *diffuse = *(m.diffuse);
+}
---
+++ RaytracingCamp2018/Material.hpp
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "Spectrum.hpp"
+
+class Material
+{
+public:
+    Material();
+    Material(const Material &material);
+    Material(Spectrum *diffuse);
+    ~Material();
+    void operator=(const Material &m);
+    Spectrum *diffuse;
+};
+
