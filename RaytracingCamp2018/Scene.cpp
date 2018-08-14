22a6e43fdd86a3495532bb3df1537ac8877228a2
Shuto <shutoreds@gamil.com>
2018-08-15 03:51:40 +0900

    add: Scene追加

---
+++ RaytracingCamp2018/Scene.cpp
@@ -0,0 +1,45 @@
+﻿#include "Scene.hpp"
+
+Scene::Scene()
+    : objs(new std::vector<IIntersectable*>())
+{
+}
+
+
+Scene::~Scene()
+{
+    for (IIntersectable* element : *objs)
+    {
+        delete element;
+    }
+    objs->clear();
+}
+
+
+void Scene::addIntersectable(IIntersectable *obj)
+{
+    objs->push_back(obj);
+}
+
+void Scene::trace(const Ray &ray, Spectrum &spectrum)
+{
+    auto nearest = Intersection();
+    findNearestInterSection(ray, nearest);
+    // ヒットしなかったので空の色
+    if (!nearest.isHit()) {
+        spectrum += Spectrum::Sky;
+        return;
+    }
+    spectrum += *nearest.mat->diffuse;
+}
+
+void Scene::findNearestInterSection(const Ray &ray, Intersection &nearest)
+{
+    for (auto i = 0; i < objs->size(); i++) {
+        auto result = Intersection();
+        objs->at(i)->intersect(ray, result);
+        if (result.t < nearest.t) {
+            nearest = result;
+        }
+    }
+}
---
+++ RaytracingCamp2018/Scene.hpp
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <vector>
+#include "IIntersectable.hpp"
+#include "Sphere.hpp"
+
+class Scene
+{
+public:
+    Scene();
+    ~Scene();
+    void addIntersectable(IIntersectable *obj);
+    void trace(const Ray &ray, Spectrum &spectrum);
+
+private:
+    std::vector<IIntersectable*> *objs;
+
+    void findNearestInterSection(const Ray &ray, Intersection &isect);
+};
+
