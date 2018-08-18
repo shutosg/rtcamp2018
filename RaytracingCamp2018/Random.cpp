51559bc33757c7e07fbbaa487197ac71fa505c0d
Shuto <shutoreds@gamil.com>
2018-08-18 23:25:13 +0900

    add: シングルトンなRandomクラスを追加

---
+++ RaytracingCamp2018/Random.cpp
@@ -0,0 +1,29 @@
+﻿#include "Random.hpp"
+
+Random::Random()
+    : mt(new std::mt19937(std::random_device()()))
+    , dist(new std::uniform_real_distribution<double>(0.0, 1.0))
+{
+}
+
+Random::~Random()
+{
+    delete mt;
+    delete dist;
+}
+
+Random & Random::get_instance()
+{
+    static Random instance;
+    return instance;
+}
+
+double Random::random()
+{
+    return (*dist)(*mt);
+}
+
+double Random::random(double from, double to)
+{
+    return from + (to - from) * (*dist)(*mt);
+}
---
+++ RaytracingCamp2018/Random.hpp
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <random>
+
+class Random
+{
+private:
+    Random();
+    ~Random();
+    std::mt19937 *mt;
+    std::uniform_real_distribution<double> *dist;
+public:
+    Random(const Random&) = delete;
+    Random& operator = (const Random&) = delete;
+    Random(Random&&) = delete;
+    Random& operator = (Random&&) = delete;
+    static Random& get_instance();
+    double random();
+    double random(double from, double to);
+};
+
